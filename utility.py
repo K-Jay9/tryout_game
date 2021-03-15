f3f3bc5d79203dd4abdda5ae2b5c15a68c7f9b4d
kjempire9 <kjempirecode@gmail.com>
2021-03-15 16:11:31 +0300

    added some utilitites

--- utility.py
+++ utility.py
@@ -8,16 +8,19 @@ import pygame as pg
 
 # impact sounds
 
-bloody = pg.mixer.Sound('./sounds/bloody.mp3')
-whack = pg.mixer.Sound('./sounds/whack.mp3')
-rock = pg.mixer.Sound('./sounds/rock.mp3')
+# initialising pygame
+pg.init()
+
+bloody = pg.mixer.Sound('sounds/bloody.wav')
+whack = pg.mixer.Sound('./sounds/whack.wav')
+rock = pg.mixer.Sound('./sounds/rock.wav')
 
 
 # game status sounds
 # level_up = pg.mixer.Sound('./sounds/level_up.mp3')
 # game_over = pg.mixer.Sound('./sounds/game_over.mp3')
 
-game_intro = pg.mixer.Sound('./sounds/futuristic_dark.mp3')
-evil_laugh = pg.mixer.Sound('./sounds/evil_laugh.mp3')
+game_intro = pg.mixer.Sound('./sounds/futuristic_dark.wav')
+evil_laugh = pg.mixer.Sound('./sounds/evil_laugh.wav')
 
 
