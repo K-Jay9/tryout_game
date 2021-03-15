93cd38a78984f7afde0ce79378432d3a30ee42bc
kjempire9 <kjempirecode@gmail.com>
2021-03-15 15:31:37 +0300

    added some game utilities

---
+++ utility.py
@@ -0,0 +1,14 @@
+
+
+'''
+This file contains game's development backend stuff
+'''
+
+import pygame as pg 
+
+bloody = pg.mixer.Sound('./sounds/bloody.mp3')
+whack = pg.mixer.Sound('./sounds/whack.mp3')
+rock = pg.mixer.Sound('./sounds/rock.mp3')
+
+level_up = pg.mixer.Sound('./sounds/level_up.mp3')
+game_over = pg.mixer.Sound('./sounds/game_over.mp3')
